4239674f2722ae6ea4f36f510a23db34d39f5774
sponsor21ghc <sponsor21ghc@gmail.com>
2023-12-28 17:21:31 -0700

    work glove info

---
+++ js/LeftOverSlopCode.js
@@ -0,0 +1,119 @@
+
+var image2 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image2.src="images/IMG_20231029_085031.jpg";
+    
+image2.width=1120;
+image2.height=980;
+image2.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image2);
+
+
+
+var image3 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image3.src="images/IMG_20231029_085054.jpg";
+    
+image3.width=1120;
+image3.height=980;
+image3.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image3);
+
+
+
+var image4 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image4.src="images/IMG_20231029_085849.jpg";
+    
+image4.width=1120;
+image4.height=980;
+image4.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image4);
+
+
+var image5 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image5.src="images/IMG_20231029_085906.jpg";
+    
+image5.width=1120;
+image5.height=980;
+image5.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image5);
+
+
+var image6 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image6.src="images/IMG_20231029_085907.jpg";
+    
+image6.width=1120;
+image6.height=980;
+image6.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image6);
+
+
+var image7 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image7.src="images/IMG_20231029_085935.jpg";
+    
+image7.width=1120;
+image7.height=980;
+image7.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image7);
+
+
+
+
+
+
+
+var image8 = document.createElement('img');
+    
+/*image1.src="images/rhino.jpg"; */
+
+
+image8.src="images/IMG_20231029_090023.jpg";
+    
+image8.width=1120;
+image8.height=980;
+image8.alt="here should be some image"
+/*image1.write("Ur Mother was a hamster");*/
+
+    
+document.body.appendChild(image8);
+
